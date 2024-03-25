3fb19ec00117ae7c47510f21b5eb1c02eb216627
Cara Teodora <carateodora21@stud.ase.ro>
2024-03-25 17:41:56 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+# import the necessary libraries
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.discriminant_analysis import StandardScaler
+import statsmodels.api as sm
+graduates = pd.read_csv('data/Graduates.csv',delimiter=';', index_col=0)
+registered = pd.read_csv('data/Registered.csv', delimiter=';', index_col=0)
+labs = pd.read_csv('data/Labs.csv', delimiter=';', index_col=0)
+population = pd.read_csv('data/Population.csv', delimiter=';', index_col=0)
+dropout_rate = pd.read_csv('data/Dropout_rate.csv', delimiter=';', index_col=0)
+poverty = pd.read_csv('data/Poverty.csv', delimiter=';', index_col=0)
